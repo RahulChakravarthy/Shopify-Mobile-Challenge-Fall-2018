71b51e08ba76625d3112983bf2282330e537c3ab
Rahul-Chakravarthy <rahul.chakravarthy101@gmail.com>
2018-04-30 22:38:18 -0400

    Created base template for APP

---
+++ app/src/main/java/com/shopify/mobilechallengefall2018/ShopifyChallengeFall2018.java
@@ -0,0 +1,13 @@
+package com.shopify.mobilechallengefall2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopifyChallengeFall2018 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopify_challenge_fall2018);
+    }
+}
